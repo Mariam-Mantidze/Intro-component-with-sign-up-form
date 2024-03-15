58c569112b394933068cae10b97ccf59c1436318
Mariam <mariam.mantidze1@gmail.com>
2024-03-15 19:05:46 +0400

    add error-icon inside inputs

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -2,50 +2,69 @@ import styled from "styled-components";
 import ErrorIcon from "/images/icon-error.svg";
 
 export default function Inputs({ values, handleChange, errors }) {
-  const inputs = [];
   return (
     <>
-      <Input
-        onChange={handleChange}
-        value={values.name}
-        errors={errors.name}
-        id="name"
-        type="text"
-        placeholder="First Name"
-      />
-      {errors.name && <ErrorMessage>{errors.name}</ErrorMessage>}
-      <Input
-        value={values.lastName}
-        errors={errors.lastName}
-        onChange={handleChange}
-        id="lastName"
-        type="text"
-        placeholder="Last Name"
-      />
-      {errors.lastName && <ErrorMessage>{errors.lastName}</ErrorMessage>}
-      <Input
-        value={values.email}
-        errors={errors.email}
-        onChange={handleChange}
-        id="email"
-        type="email"
-        placeholder="Email Adress"
-      />
-      {errors.email && <ErrorMessage>{errors.email}</ErrorMessage>}
-      <Input
-        value={values.password}
-        errors={errors.password}
-        onChange={handleChange}
-        id="password"
-        type="password"
-        placeholder="Password"
-      />
-      {errors.password && <ErrorMessage>{errors.password}</ErrorMessage>}
+      <InputContainer>
+        <Input
+          onChange={handleChange}
+          value={values.name}
+          errors={errors.name}
+          id="name"
+          type="text"
+          placeholder="First Name"
+        />
+        {errors.name && <ErrorMessage>{errors.name}</ErrorMessage>}
+        {errors.name ? <ErrorImg src={ErrorIcon} alt="Error Icon" /> : null}
+      </InputContainer>
+
+      <InputContainer>
+        <Input
+          value={values.lastName}
+          errors={errors.lastName}
+          onChange={handleChange}
+          id="lastName"
+          type="text"
+          placeholder="Last Name"
+        />
+        {errors.lastName && <ErrorMessage>{errors.lastName}</ErrorMessage>}
+        {errors.lastName ? <ErrorImg src={ErrorIcon} alt="Error Icon" /> : null}
+      </InputContainer>
+
+      <InputContainer>
+        <Input
+          value={values.email}
+          errors={errors.email}
+          onChange={handleChange}
+          id="email"
+          type="email"
+          placeholder="Email Adress"
+        />
+        {errors.email && <ErrorMessage>{errors.email}</ErrorMessage>}
+        {errors.email ? <ErrorImg src={ErrorIcon} alt="Error Icon" /> : null}
+      </InputContainer>
+
+      <InputContainer>
+        <Input
+          value={values.password}
+          errors={errors.password}
+          onChange={handleChange}
+          id="password"
+          type="password"
+          placeholder="Password"
+        />
+        {errors.password && <ErrorMessage>{errors.password}</ErrorMessage>}
+        {errors.password ? <ErrorImg src={ErrorIcon} alt="Error Icon" /> : null}
+      </InputContainer>
     </>
   );
 }
 
+const InputContainer = styled.div`
+  position: relative;
+`;
+
 const Input = styled.input`
+  width: 100%;
   padding: 15px 19px;
   border: 1px solid rgba(222, 222, 222, 1);
   border-radius: 5px;
@@ -70,4 +89,15 @@ const ErrorMessage = styled.p`
   text-align: right;
   color: rgba(255, 121, 121, 1);
   margin-top: 6px;
+`;
+
+const ErrorImg = styled.img`
+  position: absolute;
+  top: 30px;
+  right: 17px;
+
+  @media (min-width: 1400px) {
+    top: 35px;
+    right: 27px;
+  }
 `;
